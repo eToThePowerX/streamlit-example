e79383154fdadfe7a366cc65a6d5ca8b503d5d5a
eToThePowerX <75633018+eToThePowerX@users.noreply.github.com>
2022-09-08 11:59:01 +0530

    up3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,3 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Uber pickups in NYC')
-$ streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
