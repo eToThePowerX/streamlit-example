c601dafcf3f9a5f2577b8055d6a7a7d30613ad1c
eToThePowerX <75633018+eToThePowerX@users.noreply.github.com>
2022-09-08 12:18:17 +0530

    up7

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from catboost import CatBoostClassifier
+from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
