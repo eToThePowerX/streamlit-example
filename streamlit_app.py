d78bacc286a36bc528ccd6b0b5b77f7567c028cd
eToThePowerX <75633018+eToThePowerX@users.noreply.github.com>
2022-09-08 13:51:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,10 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 ##Read data
-data_set = pd.read_csv('/Dataset.txt',sep= '\t', header=None)
+data_set = pd.read_csv('Dataset.txt',sep= '\t', header=None)
 data_copy = data_set.copy()
 
-target = data_set[61]
-st.write(target)
+
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
